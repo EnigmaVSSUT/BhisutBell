fea147509c924b191348534aba698a611d0538d8
reverie <ss.saswatsahoo@gmail.com>
2018-07-21 13:34:38 +0530

    ADDED: Extracted data is sent to database

--- ExtractingScript/bhisutBell.py
+++ ExtractingScript/bhisutBell.py
@@ -1,11 +1,15 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import pandas as pd
+import requests
 
-html = urlopen("http://vssut.ac.in/").read()
-soup = BeautifulSoup(html, "lxml")
-soup.prettify()
+# html = urlopen("http://vssut.ac.in/").read()
+# soup = BeautifulSoup(html, "lxml")
+# soup.prettify()
 
-element = soup.find("table", class_="table")
-for description in element.findAll('a'):
-	print( "http://vssut.ac.in/" + description.attrs["href"] + " " + description.contents[0])
+# element = soup.find("table", class_="table")
+# for description in element.findAll('a'):
+# 	print( "http://vssut.ac.in/" + description.attrs["href"] + " " + description.contents[0])
+
+r = requests.get('https://bhisutbell.firebaseio.com/notices.json')
+print(r)
