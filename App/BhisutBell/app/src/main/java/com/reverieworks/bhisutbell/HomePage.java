ab9e47a2a28cb736b942ea2e567aae5041591521
reverie <ss.saswatsahoo@gmail.com>
2018-07-30 02:32:51 +0530

    Notification subscriber manager added

--- App/BhisutBell/app/src/main/java/com/reverieworks/bhisutbell/HomePage.java
+++ App/BhisutBell/app/src/main/java/com/reverieworks/bhisutbell/HomePage.java
@@ -1,7 +1,9 @@
 package com.reverieworks.bhisutbell;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.onesignal.OneSignal;
 
 public class HomePage extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        // OneSignal Initialization
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
     }
 }
