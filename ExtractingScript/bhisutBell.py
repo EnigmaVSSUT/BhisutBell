d286648a083b6f84c918e4abc51763ab1d8ca404
reverie <ss.saswatsahoo@gmail.com>
2018-07-30 03:01:18 +0530

    Multiple notifications for multiple notices has been implemented

--- ExtractingScript/bhisutBell.py
+++ ExtractingScript/bhisutBell.py
@@ -19,6 +19,10 @@ element = soup.find("table", class_="table")
 isTagType = type(element.findAll('td')[0]) #To exclude NavigableString from Tag. Initialized here!
 latestNotice = requests.get(latestNoticeUrl)
 
+notice_messages = []
+notice_urls = []
+notifications = []
+
 for description in element.findAll('td'):
 	value = description.contents[0]
 	if type(value) is isTagType:
@@ -29,10 +33,11 @@ for description in element.findAll('td'):
 			data['name'] = value.contents[0]
 			data['url'] = "http://vssut.ac.in/" + value.attrs["href"]
 			json_data = json.dumps(data)
-			response = requests.post(url, data=json_data)
+			#	response = requests.post(url, data=json_data)
 			print(json_data)
-			notification_message = value.contents[0]
-			notification_url = "http://vssut.ac.in/" + value.attrs["href"]
+			notifications.append(data)
+			# notice_messages.append(value.contents[0])
+			# notice_urls.append("http://vssut.ac.in/" + value.attrs["href"])
 	else:
 		data = {}
 		data['date'] = value
@@ -42,22 +47,21 @@ json_data = element.find('a').contents[0]
 json_data = json.dumps(json_data)
 response = requests.put(latestNoticeUrl, data=json_data)
 
+# send notification
+for notification in notifications:
+	header = {"Content-Type": "application/json; charset=utf-8",
+	          "Authorization": "Basic NWViNjZmOTUtMjIyZi00NDcyLTkxYmItNmMyMTM0YjA2ZWQ2"}
+
+	payload = {"app_id": "eaf72e14-0237-4e72-a0f6-abf7ca732db6",
+	           "included_segments": ["All"],
+	           "android_group": "Bhisut",
+	           "contents": {"en": notification["name"]},
+	           "url":  notification["url"],
+	           "large_icon": "http://icons.iconarchive.com/icons/fps.hu/free-christmas-flat/128/bell-icon.png"
+	           }
+	 
+	req = requests.post("https://onesignal.com/api/v1/notifications", headers=header, data=json.dumps(payload))
+	print(req.status_code, req.reason)
 
 stop = timeit.default_timer()
 print(stop - start)
-
-# send notification to everyone
-header = {"Content-Type": "application/json; charset=utf-8",
-          "Authorization": "Basic NWViNjZmOTUtMjIyZi00NDcyLTkxYmItNmMyMTM0YjA2ZWQ2"}
-
-payload = {"app_id": "eaf72e14-0237-4e72-a0f6-abf7ca732db6",
-           "included_segments": ["All"],
-           "android_group": "Bhisut",
-           "contents": {"en": notification_message},
-           "url": notification_url,
-           "large_icon": "http://icons.iconarchive.com/icons/fps.hu/free-christmas-flat/128/bell-icon.png"
-           }
- 
-req = requests.post("https://onesignal.com/api/v1/notifications", headers=header, data=json.dumps(payload))
- 
-print(req.status_code, req.reason)
